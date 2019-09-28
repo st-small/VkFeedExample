ea637c3ebbda32ea2952a76e8ea4a30cc74ce310
Станислав Шияновский <st-small@rambler.ru>
2019-09-28 20:50:03 +0300

    Initial Commit

---
+++ VkFeedExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VkFeedExample
+//
+//  Created by Станислав Шияновский on 9/28/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
